5bf1b2e8111966521a57deb5c1f3d7e79b68029e
Mad565 <104206467+Mad565@users.noreply.github.com>
2023-11-12 16:08:57 +0530

    FPS-Stride-Engine
    FPS-Stride-Engine

---
+++ MyGame/MyGame/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stride.Core.Mathematics;
+using Stride.Input;
+using Stride.Engine;
+using System.Xml;
+
+namespace MyGame
+{
+    public class Enemy : SyncScript
+    {
+        public float HP = 100.0f;
+        public Entity Need_to_Delete;
+
+        public override void Update()
+        {
+            if (HP <= 0)
+            {
+                DebugText.Print("Enemy Deleted!!!", new Int2(580, 580));
+                Need_to_Delete.Scene.Entities.Remove(Need_to_Delete);
+                Need_to_Delete.Scene = null;
+
+            }
+        }
+    }
+}
